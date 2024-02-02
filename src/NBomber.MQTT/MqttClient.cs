5d0af86f633b3561d3aa75ed4734569980ce890d
Andrey <111135827+mangystx@users.noreply.github.com>
2024-02-02 16:25:29 +0200

    .

---
+++ src/NBomber.MQTT/MqttClient.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.FSharp.Core;
+using MQTTnet.Client;
+using NBomber.Contracts;
+using Microsoft.IO;
+
+
+
+namespace NBomber.MQTT;
+
+public class MqttClient(IMqttClient client) : IDisposable
+{
+    private static readonly RecyclableMemoryStreamManager MsStreamManager = new();
+    
+    public IMqttClient Client { get; } = client;
+
+    public async Task<Response<MqttClientPublishResult>> Publish(string topic, string text)
+    {
+        var result = await client.PublishStringAsync(topic, text);
+        var payload = FSharpOption<MqttClientPublishResult>.Some(result); 
+        return new Response<MqttClientPublishResult>(result.ReasonCode.ToString(), false, text.Length, "", payload) ;
+    }
+    
+    public async Task<Response<MqttClientPublishResult>> Publish(string topic, byte[] payload)
+    {
+        var result = await client.PublishBinaryAsync(topic, payload);
+        var payloadForResponse = FSharpOption<MqttClientPublishResult>.Some(result); 
+        return new Response<MqttClientPublishResult>(result.ReasonCode.ToString(), false, payload.Length, "", payloadForResponse) ;
+    }
+    
+    public void Dispose()
+    {   
+        client.Dispose();
+    }
+}
