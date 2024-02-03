f21fde68178286e458ab77cb8b5a0e88bf714856
Andrey <111135827+mangystx@users.noreply.github.com>
2024-02-03 15:16:02 +0200

    without try catch

--- src/NBomber.MQTT/MqttClient.cs
+++ src/NBomber.MQTT/MqttClient.cs
@@ -1,58 +1,39 @@
 ﻿using Microsoft.FSharp.Core;
 using MQTTnet.Client;
 using NBomber.Contracts;
-using Microsoft.IO;
 
 namespace NBomber.MQTT;
 
 public class MqttClient(IMqttClient client) : IDisposable
 {
-    private static readonly RecyclableMemoryStreamManager MsStreamManager = new();
-    
     public IMqttClient Client { get; } = client;
 
     public async Task<Response<MqttClientPublishResult>> Publish(string topic, string text)
     {
-        try
+        var result = await client.PublishStringAsync(topic, text);
+        var payload = FSharpOption<MqttClientPublishResult>.Some(result);
+        if (result.IsSuccess)
         {
-            var result = await client.PublishStringAsync(topic, text);
-            var payload = FSharpOption<MqttClientPublishResult>.Some(result);
-            if (result.IsSuccess)
-            {
-                return new Response<MqttClientPublishResult>(statusCode: result.ReasonCode.ToString(), isError: false,
-                    sizeBytes: text.Length, message: "", payload: payload);
-            }
+            return new Response<MqttClientPublishResult>(statusCode: result.ReasonCode.ToString(), isError: false,
+                sizeBytes: text.Length, message: string.Empty, payload: payload);
+        }
             
-            return new Response<MqttClientPublishResult>(statusCode: result.ReasonCode.ToString(), isError: true,
-                sizeBytes: 0, message: result.ReasonString, payload: FSharpOption<MqttClientPublishResult>.Some(null));
-        }
-        catch (Exception e)
-        {
-            return new Response<MqttClientPublishResult>(statusCode: "128", isError: true,
-                sizeBytes: 0, message: e.Message, FSharpOption<MqttClientPublishResult>.Some(null));
-        }
+        return new Response<MqttClientPublishResult>(statusCode: result.ReasonCode.ToString(), isError: true,
+            sizeBytes: 0, message: result.ReasonString, payload: payload);
     }
     
     public async Task<Response<MqttClientPublishResult>> Publish(string topic, byte[] payload)
     {
-        try
+        var result = await client.PublishBinaryAsync(topic, payload);
+        var payloadForResponse = FSharpOption<MqttClientPublishResult>.Some(result);
+        if (result.IsSuccess)
         {
-            var result = await client.PublishBinaryAsync(topic, payload);
-            var payloadForResponse = FSharpOption<MqttClientPublishResult>.Some(result);
-            if (result.IsSuccess)
-            {
-                return new Response<MqttClientPublishResult>(statusCode: result.ReasonCode.ToString(), isError: false,
-                    sizeBytes: payload.Length, message: "", payload: payloadForResponse);
-            }
+            return new Response<MqttClientPublishResult>(statusCode: result.ReasonCode.ToString(), isError: false,
+                sizeBytes: payload.Length, message: string.Empty, payload: payloadForResponse);
+        }
             
-            return new Response<MqttClientPublishResult>(statusCode: result.ReasonCode.ToString(), isError: true,
-                sizeBytes: 0, message: result.ReasonString, payload: FSharpOption<MqttClientPublishResult>.Some(null));
-        }
-        catch (Exception e)
-        {
-            return new Response<MqttClientPublishResult>(statusCode: "128", isError: true,
-                sizeBytes: 0, message: e.Message, FSharpOption<MqttClientPublishResult>.Some(null));
-        }
+        return new Response<MqttClientPublishResult>(statusCode: result.ReasonCode.ToString(), isError: true,
+            sizeBytes: 0, message: result.ReasonString, payload: payloadForResponse);
     }
     
     public void Dispose()
