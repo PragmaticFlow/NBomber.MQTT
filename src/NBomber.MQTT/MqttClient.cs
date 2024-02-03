bd2b34bc3b14622f23ee6bd232f1effe867f6e02
Andrey <111135827+mangystx@users.noreply.github.com>
2024-02-03 21:44:51 +0200

    MqttClient.cs updated with connect and disconnect methods

--- src/NBomber.MQTT/MqttClient.cs
+++ src/NBomber.MQTT/MqttClient.cs
@@ -8,6 +8,41 @@ public class MqttClient(IMqttClient client) : IDisposable
 {
     public IMqttClient Client { get; } = client;
 
+    public async Task<Response<MqttClientConnectResult>> Connect(MqttClientOptions options,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await client.ConnectAsync(options, cancellationToken);
+        if (result.ResultCode == MqttClientConnectResultCode.Success)
+        {
+            return new Response<MqttClientConnectResult>(statusCode: result.ResultCode.ToString(), isError: false,
+                0, message: string.Empty, payload: result);
+        }
+        
+        return new Response<MqttClientConnectResult>(statusCode: result.ResultCode.ToString(), isError: true,
+            0, message: $"Reason string: {result.ReasonString}\nResponse information: {result.ResponseInformation}", 
+            payload: result);
+    }
+    
+    public async Task<Response<MqttClientConnectResult>> Connect(string socketServer, 
+        CancellationToken cancellationToken = default)
+    {
+        var options = new MqttClientOptionsBuilder().WithWebSocketServer(optionsBuilder => 
+        {
+            optionsBuilder.WithUri(socketServer);
+        }).WithCleanSession().Build();
+        
+        var result = await client.ConnectAsync(options, cancellationToken);
+        if (result.ResultCode == MqttClientConnectResultCode.Success)
+        {
+            return new Response<MqttClientConnectResult>(statusCode: result.ResultCode.ToString(), isError: false,
+                0, message: string.Empty, payload: result);
+        }
+        
+        return new Response<MqttClientConnectResult>(statusCode: result.ResultCode.ToString(), isError: true,
+            0, message: $"Reason string: {result.ReasonString}\nResponse information: {result.ResponseInformation}", 
+            payload: result);
+    }
+
     public async Task<Response<MqttClientPublishResult>> Publish(string topic, string text)
     {
         var result = await client.PublishStringAsync(topic, text);
@@ -34,6 +69,33 @@ public class MqttClient(IMqttClient client) : IDisposable
             
         return new Response<MqttClientPublishResult>(statusCode: result.ReasonCode.ToString(), isError: true,
             sizeBytes: 0, message: result.ReasonString, payload: payloadForResponse);
+    }
+
+    public async Task<Response<object>> Disconnect()
+    {
+        await client.DisconnectAsync();
+        return new Response<object>(statusCode: string.Empty, isError: !client.IsConnected, sizeBytes: 0,
+            message: string.Empty, payload: null);
+    }
+    
+    public async Task<Response<object>> Disconnect(MqttClientDisconnectOptions options, 
+        CancellationToken cancellationToken = default)
+    {
+        await client.DisconnectAsync(options, cancellationToken);
+        return new Response<object>(statusCode: string.Empty, isError: !client.IsConnected, sizeBytes: 0,
+            message: string.Empty, payload: null);
+    }
+    
+    public async Task<Response<object>> Disconnect(MqttClientDisconnectOptionsReason reason, string reasonString, 
+        CancellationToken cancellationToken = default)
+    {
+        var options = new MqttClientDisconnectOptions();
+        options.ReasonString = reasonString;
+        options.Reason = reason;
+        
+        await client.DisconnectAsync(options, cancellationToken);
+        return new Response<object>(statusCode: string.Empty, isError: !client.IsConnected, sizeBytes: 0,
+            message: string.Empty, payload: null);
     }
     
     public void Dispose()
