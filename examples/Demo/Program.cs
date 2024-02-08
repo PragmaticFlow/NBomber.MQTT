dafb17b122d698ce692648bbc9895df30abed841
Anton Moldovan <antyadev@gmail.com>
2024-02-08 22:29:47 +0200

    Mqtt plugin (#2)

---
+++ examples/Demo/Program.cs
@@ -0,0 +1,58 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using NBomber.CSharp;
+using MqttClient = NBomber.MQTT.MqttClient;
+
+new MqttHelloTest().Run();
+
+public class MqttHelloTest
+{
+    public void Run()
+    {
+        var payload = "hello";
+
+        var scenario = Scenario.Create("mqtt_hello_scenario", async ctx =>
+        {
+            var topic = $"/clients/{ctx.ScenarioInfo.ThreadId}";
+            var mqttClient = new MqttClient(new MqttFactory().CreateMqttClient());
+
+            var connect = await Step.Run("connect", ctx, async () =>
+            {
+                var options = new MqttClientOptionsBuilder().WithWebSocketServer(optionsBuilder =>
+                {
+                    optionsBuilder.WithUri("ws://localhost:8083/mqtt");
+                }).Build();
+                
+                return await mqttClient.Connect(options);
+            });
+
+            var subscribe = await Step.Run("subscribe", ctx, async () =>
+                await mqttClient.Subscribe(topic));
+
+            var publish = await Step.Run("publish", ctx, async () =>
+            {
+                var msg = new MqttApplicationMessageBuilder().WithTopic(topic)
+                    .WithPayload(payload).Build();
+                
+                return await mqttClient.Publish(msg);
+            });
+
+            var receive = await Step.Run("receive", ctx, async () => 
+                await mqttClient.Receive());
+
+            var disconnect = await Step.Run("disconnect", ctx, async () =>
+                await mqttClient.Disconnect());
+
+            return Response.Ok();
+        })
+        .WithoutWarmUp()
+        .WithLoadSimulations(
+            Simulation.KeepConstant(1, TimeSpan.FromSeconds(30))
+        );
+
+        NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+    }
+}
+
---
+++ src/NBomber.MQTT/MqttClient.cs
@@ -0,0 +1,84 @@
+﻿using System.Threading.Channels;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Packets;
+using MQTTnet.Protocol;
+using NBomber.Contracts;
+using NBomber.CSharp;
+
+namespace NBomber.MQTT;
+
+public class MqttClient : IDisposable
+{
+    public IMqttClient Client { get; }
+
+    private readonly Channel<Response<MqttApplicationMessage>> _channel;
+    
+    public MqttClient (IMqttClient client)
+    {
+        Client = client;
+        _channel = Channel.CreateUnbounded<Response<MqttApplicationMessage>>();
+        
+        Client.ApplicationMessageReceivedAsync += msg =>
+        {
+            var response = Response.Ok(sizeBytes:msg.ApplicationMessage.PayloadSegment.Count, payload:msg.ApplicationMessage);
+            
+            return _channel.Writer.WriteAsync(response).AsTask();
+        };
+    }
+    
+    public async Task<Response<MqttClientConnectResult>> Connect(MqttClientOptions options,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await Client.ConnectAsync(options, cancellationToken);
+        if (result.ResultCode == MqttClientConnectResultCode.Success)
+        {
+            return Response.Ok(statusCode: result.ResultCode.ToString(), payload: result);
+        }
+        
+        return Response.Fail(payload: result, statusCode: result.ResultCode.ToString(), 
+            message: $"Reason string: {result.ReasonString}\nResponse information: {result.ResponseInformation}");
+    }
+
+    public async Task<Response<MqttClientSubscribeResult>> Subscribe(
+        string topic,
+        MqttQualityOfServiceLevel qualityOfServiceLevel = MqttQualityOfServiceLevel.AtMostOnce,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await Client.SubscribeAsync(topic, qualityOfServiceLevel, cancellationToken);
+        
+        return Response.Ok(payload: result);
+    }
+    
+    public async Task<Response<MqttClientPublishResult>> Publish(MqttApplicationMessage applicationMessage,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await Client.PublishAsync(applicationMessage, cancellationToken);
+        if (result.IsSuccess)
+        {
+            return Response.Ok(payload: result, statusCode: result.ReasonCode.ToString(),
+                sizeBytes: applicationMessage.PayloadSegment.Count);
+        }
+            
+        return Response.Ok(payload: result, statusCode: result.ReasonCode.ToString(), message: result.ReasonString);
+    }
+
+    public ValueTask<Response<MqttApplicationMessage>> Receive() => _channel.Reader.ReadAsync();
+    
+    public async Task<Response<object>> Disconnect(
+        MqttClientDisconnectOptionsReason reason = MqttClientDisconnectOptionsReason.NormalDisconnection,
+        string reasonString = null,
+        uint sessionExpiryInterval = 0,
+        List<MqttUserProperty> userProperties = null,
+        CancellationToken cancellationToken = default)
+    {
+        await Client.DisconnectAsync(reason, reasonString, sessionExpiryInterval, userProperties, cancellationToken);
+        
+        return Client.IsConnected ? Response.Fail() : Response.Ok();
+    }
+    
+    public void Dispose()
+    {   
+        Client.Dispose();
+    }
+}
