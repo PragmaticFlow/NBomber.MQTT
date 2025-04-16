c226688d458c9043717f287cce780da1a856e954
User <oleksandr.palamarchuk1310@gmail.com>
2025-04-16 10:58:06 +0300

    Add the Independent Actors example

---
+++ examples/IndependentActors/ConsumeScenario.cs
@@ -0,0 +1,51 @@
+﻿using NBomber.Data;
+using NBomber.Contracts;
+using NBomber.CSharp;
+using MQTTnet.Client;
+using MQTTnet;
+using MqttClient = NBomber.MQTT.MqttClient;
+using MQTTnet.Formatter;
+
+namespace IndependentActors;
+
+public class ConsumeScenario
+{
+    public ScenarioProps Create()
+    {
+        byte[] payload = Data.GenerateRandomBytes(200);
+        MqttClient mqttClient = null;
+
+        return Scenario.Create("consume_scenario", async ctx =>
+        {
+            var message = await mqttClient.Receive(ctx.ScenarioCancellationToken);
+
+            // Final latency is computed by subtracting the current time from the timestamp in the header.
+            var timestampMs = long.Parse(message.Payload.Value.UserProperties.FirstOrDefault(prop => prop.Name == "timestamp").Value);
+            var latency = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - timestampMs;
+
+            return Response.Ok(customLatencyMs: latency);
+        })
+        .WithoutWarmUp()
+        .WithLoadSimulations(
+            Simulation.KeepConstant(1, TimeSpan.FromSeconds(30))
+        )
+        .WithInit(async ctx =>
+        {
+            var clientId = $"mqtt_consumer";
+            var options = new MqttClientOptionsBuilder()
+                .WithWebSocketServer(options => { options.WithUri("ws://localhost:8083/mqtt"); })
+                .WithClientId(clientId)
+                .WithProtocolVersion(MqttProtocolVersion.V500)
+                .Build();
+
+            mqttClient = new MqttClient(new MqttFactory().CreateMqttClient());
+            await mqttClient.Connect(options);
+
+            await mqttClient.Subscribe($"/clients/independentActors");
+        })
+        .WithClean(async ctx =>
+        {
+            await mqttClient.Disconnect();
+        });
+    }
+}
