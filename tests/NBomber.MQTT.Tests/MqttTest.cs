7c09b7b8fdbe3eeaaa1f52a7e22b7dfbc6ab096b
User <oleksandr.palamarchuk1310@gmail.com>
2025-05-05 15:06:30 +0300

    Add client pool example for test

--- tests/NBomber.MQTT.Tests/MqttTest.cs
+++ tests/NBomber.MQTT.Tests/MqttTest.cs
@@ -1,4 +1,6 @@
 using MQTTnet;
+using MQTTnet.Protocol;
+using NBomber;
 using NBomber.CSharp;
 using NBomber.Data;
 using MqttClient = NBomber.MQTT.MqttClient;
@@ -10,44 +12,65 @@ public class MqttTest
     [Fact]
     public void EndToEnd()
     {
+        var clientPool = new ClientPool<MqttClient>();
         var payload = Data.GenerateRandomBytes(200);
 
         var scenario = Scenario.Create("mqtt_scenario", async ctx =>
         {
-            var topic = $"/clients/{ctx.ScenarioInfo.InstanceId}";
-            var mqttClient = new MqttClient(new MqttClientFactory().CreateMqttClient());
-
-            var connect = await Step.Run("connect", ctx, async () =>
-            {
-                var options = new MqttClientOptionsBuilder()
-                    .WithWebSocketServer(options => { options.WithUri("ws://localhost:8083/mqtt"); })
-                    .Build();
-
-                return await mqttClient.Connect(options);
-            });
-
-            var subscribe = await Step.Run("subscribe", ctx, async () => await mqttClient.Subscribe(topic));
+            var mqttClient = clientPool.GetClient(ctx.ScenarioInfo);
 
             var publish = await Step.Run("publish", ctx, async () =>
             {
+                var topic = $"/clients/{ctx.ScenarioInfo.InstanceId}";
                 var msg = new MqttApplicationMessageBuilder()
                     .WithTopic(topic)
                     .WithPayload(payload)
+                    .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtMostOnce)
                     .Build();
 
                 return await mqttClient.Publish(msg);
             });
 
-            var receive = await Step.Run("receive", ctx, async () => await mqttClient.Receive(ctx.ScenarioCancellationToken));
-
-            var disconnect = await Step.Run("disconnect", ctx, async () => await mqttClient.Disconnect());
+            var receive = await Step.Run("receive", ctx, async () =>
+            {
+                var response = await mqttClient.Receive(ctx.ScenarioCancellationToken);
+                return response;
+            });
 
             return Response.Ok();
         })
         .WithWarmUpDuration(TimeSpan.FromSeconds(5))
-        .WithLoadSimulations(
-            Simulation.KeepConstant(10, TimeSpan.FromSeconds(5))
-        );
+        .WithLoadSimulations(Simulation.KeepConstant(10, TimeSpan.FromSeconds(5)))
+        .WithInit(async context =>
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                var topic = $"/clients/mqtt_scenario_{i}";
+                var clientId = $"mqtt_client_{i}";
+                var options = new MqttClientOptionsBuilder()
+                    .WithTcpServer("localhost")
+                    .WithClientId(clientId)
+                    .Build();
+
+                var mqttClient = new MqttClient(new MqttClientFactory().CreateMqttClient());
+                var connectResult = await mqttClient.Connect(options);
+
+                if (!connectResult.IsError)
+                {
+                    await mqttClient.Subscribe(topic, MqttQualityOfServiceLevel.AtMostOnce);
+                    clientPool.AddClient(mqttClient);
+                }
+                else
+                    throw new Exception("client can't connect to the MQTT broker");
+
+                await Task.Delay(10);
+            }
+        })
+        .WithClean(ctx =>
+        {
+            clientPool.DisposeClients(client => client.Dispose());
+            return Task.CompletedTask;
+        });        
 
         var stats = NBomberRunner
             .RegisterScenarios(scenario)
